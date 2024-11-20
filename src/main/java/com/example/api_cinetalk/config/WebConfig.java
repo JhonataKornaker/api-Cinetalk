aceae594f52133e006be74fa84e99c36bad83fc3
Jhonata Kornaker <jhonataalberto@outlook.com>
2024-11-20 20:41:41 -0300

    configuração data base

--- src/main/java/com/example/api_cinetalk/config/WebConfig.java
+++ src/main/java/com/example/api_cinetalk/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.0.120:8080", "http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
